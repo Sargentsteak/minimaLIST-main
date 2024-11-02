de7407c0040b55bde5166b79c0a3dfbad4449906
Piyush Barhanpurkar <66437656+Sargentsteak@users.noreply.github.com>
2024-11-03 00:15:45 +0530

    Update FooterPlain.jsx

--- frontend/src/components/partials/FooterPlain.jsx
+++ frontend/src/components/partials/FooterPlain.jsx
@@ -4,7 +4,7 @@ export function FooterPlain() {
   return (
     <Footer height={60} p="md">
       <Flex justify={"space-between"}>
-        <Text c={"dimmed"}>Designed and Developed by: Piyush Terkar.</Text>
+        <Text c={"dimmed"}>Designed and Developed by: Piyush </Text>
         <Text c={"dimmed"}>Minimalist &copy; 2023</Text>
       </Flex>
     </Footer>
